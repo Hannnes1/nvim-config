cd70fd31ae3c13e2f986f894957e4c6bae3a013a
Hannes <hannes.hultergard@gmail.com>
2024-02-29 08:08:44 +0100

    Remove duplicate dart lsp setup

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -19,8 +19,6 @@ lsp_zero.on_attach(function(client, bufnr)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
-lsp_zero.build_options('dartls', {})
-
 -- to learn how to use mason.nvim with lsp-zero
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
 require('mason').setup({})
