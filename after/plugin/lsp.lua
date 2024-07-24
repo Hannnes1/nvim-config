2039be23f54d9f9b375995dd7633792ec53ff44c
Hannes Hultergård <hannes.hultergard@gmail.com>
2024-07-24 09:35:20 +0200

    Add more required LSPs

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -23,7 +23,7 @@ end)
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = { 'tsserver', 'eslint', 'lua_ls', 'gopls' },
+  ensure_installed = { 'tsserver', 'eslint', 'lua_ls', 'gopls', 'html', 'htmx', 'cssls', 'templ', 'tailwindcss' },
   handlers = {
     lsp_zero.default_setup,
     lua_ls = function()
