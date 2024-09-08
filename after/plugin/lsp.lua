33dade9cbdbeeb06b710d5aa4a79d26d098895c8
Hannes Hultergård <hannes.hultergard@gmail.com>
2024-09-08 11:46:45 +0200

    Use ^j for confirming LSP option (again)

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -61,7 +61,7 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
     ['<C-Space>'] = cmp.mapping.complete(),
   }),
 })
